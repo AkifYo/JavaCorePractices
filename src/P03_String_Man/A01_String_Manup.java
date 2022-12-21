195b9961ffa67c65ca20688aae31d6a1416b3785
akif <70863898+akif-ned@users.noreply.github.com>
2022-12-21 11:26:24 +0100

    last commit

---
+++ src/P03_String_Man/A01_String_Manup.java
@@ -0,0 +1,26 @@
+package P03_String_Man;
+
+import java.util.Scanner;
+
+public class A01_String_Manup {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Please type en text");
+        String userText= scan.nextLine();
+
+        recurringText(userText);
+    }
+
+    private static void recurringText(String text) {
+        String emptyBox="";
+        for (int i = text.length()-1; i >=0 ; i--) {
+            emptyBox+=text.substring(i,i+1);
+
+        }
+        if (text.equalsIgnoreCase(emptyBox)){
+            System.out.println("Your test is Palindrome");
+        } else {
+            System.out.println("Your test IS NOT Palindrome");
+        }
+    }
+}
