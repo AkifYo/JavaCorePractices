7cc382ab6ffe3d7cc08aa986f1b32086ff67e6c4
akif <70863898+akif-ned@users.noreply.github.com>
2023-01-31 22:36:06 +0100

    setup

--- src/test/java/pages/AlbertHeijnHomePage.java
+++ src/test/java/pages/AlbertHeijnHomePage.java
@@ -1,4 +1,9 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+
 public class AlbertHeijnHomePage {
+    public AlbertHeijnHomePage() {
+        PageFactory.initElements(Driver.getDriver,this);
+    }
 }
