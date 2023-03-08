1c8266240c56122fcd1b6bfa47f96cde2e12ed5a
akif <70863898+akif-ned@users.noreply.github.com>
2023-03-08 21:15:28 +0100

    last

--- src/P05_Math/A01_PowerOftheNumber.java
+++ src/P05_Math/A01_PowerOftheNumber.java
@@ -1,7 +1,19 @@
 package P05_Math;
 
+import java.util.Scanner;
+
 public class A01_PowerOftheNumber {
     public static void main(String[] args) {
+        // 2^0=1
+        //2^1=2
+        //2^2=4
+        //2^3=8
+        //2^4=16
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Please enter power value:  ");
+        int powerN=scan.nextInt();
 
+        System.out.println("Please enter the limit value: ");
+        int limit=scan.nextInt();
     }
 }
